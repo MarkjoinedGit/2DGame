5a511f89217436a4abbd6db85ec94c2b058dac52
TruongAnn2003 <nguyentruongan281203@gmail.com>
2023-10-28 18:01:24 +0700

    highlight item

--- Assets/ItemCollector.cs
+++ Assets/ItemCollector.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class ItemCollector : MonoBehaviour
 {
+    Rigidbody2D rb;
+    Animator animator;
+    Collider2D col;
+    SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
-  
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        col = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("puzzleObject"))
-        {
-           
-        }    
+        spriteRenderer.color = new Color(252f / 255f, 7f / 255f, 7f / 255f, 0.87f);
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        spriteRenderer.color = Color.white;
     }
 }
