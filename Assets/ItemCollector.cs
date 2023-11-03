8c32a734db3f7ac2819a5e9d5c75a0471daf6a85
TruongAnn2003 <nguyentruongan281203@gmail.com>
2023-11-03 15:57:58 +0700

    change color

--- Assets/ItemCollector.cs
+++ Assets/ItemCollector.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ItemCollector : MonoBehaviour
 {
-    Rigidbody2D rb;
-    Animator animator;
-    Collider2D col;
-    SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Collider2D col;
+    private SpriteRenderer spriteRenderer;
+    private Color highlightColor = Color.white;
+    private Color originalColor;
+    private Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,9 @@ public class ItemCollector : MonoBehaviour
         animator = GetComponent<Animator>();
         col = GetComponent<Collider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rend = GetComponent<Renderer>();
+        originalColor = rend.material.color;
+        highlightColor = HexToColor("#FFF500");
     }
 
     // Update is called once per frame
@@ -25,12 +31,32 @@ public class ItemCollector : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        spriteRenderer.color = new Color(252f / 255f, 7f / 255f, 7f / 255f, 0.87f);
+        //spriteRenderer.color = new Color(252f / 255f, 7f / 255f, 7f / 255f, 0.87f);
+        Highlight();
 
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        spriteRenderer.color = Color.white;
+        //spriteRenderer.color = Color.white;
+        Unhighlight();
+    }
+    public void Highlight()
+    {
+        rend.material.color = highlightColor;
+    }
+
+    public void Unhighlight()
+    {
+        rend.material.color = originalColor;
+    }
+    Color HexToColor(string hex)
+    {
+        Color color = Color.black;
+        if (ColorUtility.TryParseHtmlString(hex, out color))
+        {
+            return color;
+        }
+        return Color.black; // Trả về màu đen nếu chuyển đổi không thành công
     }
 }
