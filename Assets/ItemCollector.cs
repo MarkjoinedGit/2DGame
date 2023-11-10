c10f37a6a208b2d5792739b6e0b55cf482e70e7f
TruongAnn2003 <nguyentruongan281203@gmail.com>
2023-11-10 13:30:03 +0700

    finish

--- Assets/ItemCollector.cs
+++ Assets/ItemCollector.cs
@@ -1,62 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ItemCollector : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private Animator animator;
-    private Collider2D col;
-    private SpriteRenderer spriteRenderer;
     private Color highlightColor = Color.white;
     private Color originalColor;
     private Renderer rend;
+    public float delaySecond = 1;
+    public string nameScene = "main";
+    public KeyCode selectKey = KeyCode.KeypadEnter;
+    private bool isSelected;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        col = GetComponent<Collider2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
         rend = GetComponent<Renderer>();
         originalColor = rend.material.color;
         highlightColor = HexToColor("#FFF500");
+        isSelected = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.GetKey(selectKey))
+        { 
+           isSelected=true;
+        }    
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //spriteRenderer.color = new Color(252f / 255f, 7f / 255f, 7f / 255f, 0.87f);
-        Highlight();
+
+        if (collision.gameObject.CompareTag("Player") )
+        {
+            
+            Highlight();
+            //collision.gameObject.SetActive(false);
+            if(isSelected) 
+            { 
+                ModeSelect();
+            }
+        }
 
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        //spriteRenderer.color = Color.white;
         Unhighlight();
     }
-    public void Highlight()
+    private void Highlight()
     {
         rend.material.color = highlightColor;
     }
 
-    public void Unhighlight()
+    private void Unhighlight()
     {
         rend.material.color = originalColor;
     }
-    Color HexToColor(string hex)
+    private Color HexToColor(string hex)
     {
         Color color = Color.black;
         if (ColorUtility.TryParseHtmlString(hex, out color))
         {
             return color;
         }
-        return Color.black; // Trả về màu đen nếu chuyển đổi không thành công
+        return Color.black;
+    }
+    public void ModeSelect()
+    {
+        StartCoroutine(LoadAfterDelay());
+    }
+    IEnumerator LoadAfterDelay()
+    {
+        yield return new WaitForSeconds(delaySecond);
+        SceneManager.LoadScene(nameScene);
     }
 }
